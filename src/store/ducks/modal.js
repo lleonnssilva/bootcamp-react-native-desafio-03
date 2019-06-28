6f10b040e07656c69b9e39ca2d706744daacb2c2
lleonnssilva <leoguaruleo@gmail.com>
2019-06-28 11:48:47 -0300

    Primeiro commit

---
+++ src/store/ducks/modal.js
@@ -0,0 +1,37 @@
+export const Types = {
+  SHOW: 'modal/SHOW',
+  HIDE: 'modal/HIDE',
+};
+
+const INITIAL_STATE = {
+  visible: false,
+  coordinate: null,
+};
+
+export default function modal(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case Types.SHOW:
+      return {
+        visible: true,
+        coordinate: action.payload.coordinate,
+      };
+    case Types.HIDE:
+      return {
+        visible: false,
+        coordinate: null,
+      };
+    default:
+      return state;
+  }
+}
+
+export const Creators = {
+  showModal: coordinate => ({
+    type: Types.SHOW,
+    payload: { coordinate },
+  }),
+
+  hideModal: () => ({
+    type: Types.HIDE,
+  }),
+};
